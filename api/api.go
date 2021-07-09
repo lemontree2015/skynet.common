49489d188b1448e758621d8d3b0b33602826e7b3
lemontree <415593463@qq.com>
2021-07-10 00:17:29 +0800

    v1

--- api/api.go
+++ api/api.go
@@ -2,6 +2,8 @@ package api
 
 import (
 	"github.com/lemontree2015/skynet"
+	"github.com/lemontree2015/skynet.common/gproto"
+	"github.com/lemontree2015/skynet.common/server_client"
 	"github.com/lemontree2015/skynet.common/session_client"
 )
 
@@ -9,15 +11,20 @@ import (
 // Server APIs
 /////////////////
 
-// 备注:
-// 通过判断返回的*skynet.ServiceKey是否为nil来判断是否删除了OldServiceKey
-func DelSessionWithSessionId(account string, sessionId uint64) (*skynet.ServiceKey, uint64, error) {
-	return session_client.DelSessionWithSessionId(account, sessionId)
+// Route一条KickoffNotify消息到目标GIM Server[N] Service
+func RemoteRouteKickoffNotify(serviceKey *skynet.ServiceKey, account string, sessionId uint64, gProtoKickOffNotify *gproto.GProtoKickOffNotify) error {
+	return server_client.KickoffNotify(serviceKey, account, sessionId, gProtoKickOffNotify)
 }
 
 /////////////////
 // Session APIs
 /////////////////
+
+// 备注:
+// 通过判断返回的*skynet.ServiceKey是否为nil来判断是否删除了OldServiceKey
+func DelSessionWithSessionId(account string, sessionId uint64) (*skynet.ServiceKey, uint64, error) {
+	return session_client.DelSessionWithSessionId(account, sessionId)
+}
 
 // 备注:
 // 通过判断返回的*skynet.ServiceKey是否为nil来判断是否有对应的Session存在

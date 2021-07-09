2b22545da5d5c6e5d17f023a74586f4c7d05ed82
lemontree <415593463@qq.com>
2021-07-10 00:12:28 +0800

    v1

--- api/api.go
+++ api/api.go
@@ -24,3 +24,9 @@ func DelSessionWithSessionId(account string, sessionId uint64) (*skynet.ServiceK
 func TouchSession(account string) (*skynet.ServiceKey, uint64, error) {
 	return session_client.TouchSession(account)
 }
+
+// 备注:
+// 通过判断返回的*skynet.ServiceKey是否为nil来判断是否有OldServiceKey
+func SetSession(account string, serviceKey *skynet.ServiceKey, remoteSessionId uint64) (*skynet.ServiceKey, uint64, error) {
+	return session_client.SetSession(account, serviceKey, remoteSessionId)
+}
